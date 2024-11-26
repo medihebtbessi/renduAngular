a4dac111cfcac9c92f900d36c86374556f54519f
emnarbii <emna.rbii@esprit.tn>
2024-11-26 16:38:19 +0100

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductComponent } from './product/product.component';
+import { ResidencesComponent } from './residences/residences.component';
+import { WishListComponent } from './wish-list/wish-list.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ResidenceDetailsComponent } from './residence-details/residence-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'products', component: ProductComponent },
+  {
+    path: 'residences',
+    component: ResidencesComponent
+    // children: [
+    //   { path: 'wishList', component: WishListComponent },
+    //   { path: 'productList', component: ProductComponent },
+    // ],
+  },
+  {path:'residences/:id',component:ResidenceDetailsComponent},
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,22 @@ import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
 import { FormsModule } from '@angular/forms';
 import { ResidencesComponent } from './residences/residences.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { WishListComponent } from './wish-list/wish-list.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ResidenceDetailsComponent } from './residence-details/residence-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
     ResidencesComponent,
+    NavbarComponent,
+    WishListComponent,
+    HomeComponent,
+    NotFoundComponent,
+    ResidenceDetailsComponent,
   ],
   imports: [
     BrowserModule,
